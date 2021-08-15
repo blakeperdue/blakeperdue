a97b700e3142453d30b09bff4763a5e3f6b3b081
Blake Perdue <blake@perdue.me>
2021-08-15 01:11:35 -0400

    Update console byline

--- src/components/ConsoleByline/ConsoleByline.js
+++ src/components/ConsoleByline/ConsoleByline.js
@@ -3,7 +3,8 @@ const ConsoleByline = () => {
   if (typeof window !== 'undefined' && window.console) {
     setTimeout(
       () => {
-        console.log('%c(ʘ‿ʘ)╯', 'color: #FF004B; font-size: 56px;')
+        // (ʘ‿ʘ)╯
+        console.log('%c(｡◕‿◕｡)\'ﾉ"', 'color: #FF004B; font-size: 56px;')
         console.log('%c' + atob('aGlAYmxha2VwZXJkdWUuY29t'), 'color: #fff; background-color:#FF004B; padding:8px 12px; border-radius: 5px; font-size: 24px; line-height:30px;')
       },
       1000
